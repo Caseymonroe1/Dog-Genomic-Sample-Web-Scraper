de9070da693ac5048f55692f31f7d20e15a85be9
Caseymonroe1 <tun04492@temple.edu>
2022-06-01 23:16:15 -0400

    Delete cleanupspreadsheet.py

--- cleanupspreadsheet.py
+++
@@ -1,8 +0,0 @@
-import xlwt
-import xlrd
-book = xlrd.open_workbook('samples.xls')
-first_sheet = book.sheet_by_index(0)
-print(first_sheet.col_values(1))
-first_sheet.write(0, 6, "NIF")
-first_sheet.write(0, 6, "Points scored")
-print(first_sheet.cell_value(0, 6))
