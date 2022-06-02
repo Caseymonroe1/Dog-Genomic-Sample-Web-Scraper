d1aeda6d176a7e03fa49be580461209c40dedfb7
Caseymonroe1 <tun04492@temple.edu>
2022-06-01 23:14:04 -0400

    Update DogGenomics.R

--- DogGenomics.R
+++ DogGenomics.R
@@ -5,7 +5,6 @@ library(tidyverse)
 library(plotrix)
 library(dplyr)
 library(tidytext)
-
 #Setting parameters
 par(mar=c(5,11,4,1))
 par(mfrow=c(2,2))
@@ -22,15 +21,18 @@ lbls <- paste(names(mytable), "\n", mytable, sep="")
 
 
 #Making plots 
-pie(mytable, labels = lbls,
-    explode=.1,
-    col=c("Red","Blue","Grey"),
-    main="Pie Chart of Listed Gender")
+pie3D(mytable, labels = lbls,
+      cex=1.2,
+      rad=0.9,
+      col=c("Red","Blue","Grey"),
+      main="Pie Chart of Listed Gender"
+      )
 
 
 barplot(sort(breedbars, decreasing = TRUE)[1:10],
         col="BLACK",
         xlab="Occurences",
+        
         xlim=c(0,350),
         las=1,
         horiz=TRUE,
@@ -38,5 +40,22 @@ barplot(sort(breedbars, decreasing = TRUE)[1:10],
 )
 
 barplot(idats, 
+        xlab="IDAT or not",
+        ylab="Frequency",
+        main="Frequency of IDAT vs Not IDAT",
         ylim=c(0,2500),
+        col=c("blue","black")
 )
+percentages<-c(65.6,34.4)
+
+barplot(percentages,
+        main="IDAT vs Not IDAT with percentage",
+        ylab="Percentage",
+        names.arg=c("IDAT","Not IDAT"),
+        col=c("blue","black"),
+        ylim=c(0,100)
+)
+# Clear everything
+detach("package:datasets", unload = TRUE)
+dev.off()
+cat("\014")
